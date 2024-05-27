0aa6d9db16f4c4f903df09407bd94018e18d3b3f
ashotisraelyan <ashotisraelyan@gmail.com>
2024-05-27 21:25:47 +0400

    feat: pdf render screen finalized

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,25 +1,31 @@
 'use client';
 
-import { ChevronDown, ChevronUp, Loader2, RotateCcw, Search } from 'lucide-react';
-import { FC, useState } from 'react';
+import { ChevronDown, ChevronUp, Loader2, RotateCw, Search } from 'lucide-react';
 import { Document, Page, pdfjs } from 'react-pdf';
-import { useResizeDetector } from 'react-resize-detector';
 
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 import { useToast } from './ui/use-toast';
+
+import { useResizeDetector } from 'react-resize-detector';
 import { Button } from './ui/button';
 import { Input } from './ui/input';
-import { z } from 'zod';
+import { useState } from 'react';
+
 import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
 import { zodResolver } from '@hookform/resolvers/zod';
 import { cn } from '@/lib/utils';
 import {
 	DropdownMenu,
-	DropdownMenuItem,
 	DropdownMenuContent,
+	DropdownMenuItem,
 	DropdownMenuTrigger,
 } from './ui/dropdown-menu';
+
+import SimpleBar from 'simplebar-react';
+import PdfFullscreen from './PdfFullscreen';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -27,15 +33,14 @@ interface PdfRendererProps {
 	url: string;
 }
 
-const PdfRenderer: FC<PdfRendererProps> = ({ url }) => {
+const PdfRenderer = ({ url }: PdfRendererProps) => {
+	const { toast } = useToast();
+
 	const [numPages, setNumPages] = useState<number>();
 	const [currPage, setCurrPage] = useState<number>(1);
 	const [scale, setScale] = useState<number>(1);
 	const [rotation, setRotation] = useState<number>(0);
 	const [renderedScale, setRenderedScale] = useState<number | null>(null);
-
-	const { toast } = useToast();
-	const { width, ref } = useResizeDetector();
 
 	const isLoading = renderedScale !== scale;
 
@@ -57,6 +62,10 @@ const PdfRenderer: FC<PdfRendererProps> = ({ url }) => {
 		resolver: zodResolver(CustomPageValidator),
 	});
 
+	console.log(errors);
+
+	const { width, ref } = useResizeDetector();
+
 	const handlePageSubmit = ({ page }: TCustomPageValidator) => {
 		setCurrPage(Number(page));
 		setValue('page', String(page));
@@ -67,13 +76,13 @@ const PdfRenderer: FC<PdfRendererProps> = ({ url }) => {
 			<div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
 				<div className="flex items-center gap-1.5">
 					<Button
-						disabled={numPages === undefined || currPage <= 1}
-						variant="ghost"
-						aria-label="previous page"
+						disabled={currPage <= 1}
 						onClick={() => {
-							setCurrPage((prev) => (prev - 1 >= 1 ? prev - 1 : prev));
+							setCurrPage((prev) => (prev - 1 > 1 ? prev - 1 : 1));
 							setValue('page', String(currPage - 1));
 						}}
+						variant="ghost"
+						aria-label="previous page"
 					>
 						<ChevronDown className="h-4 w-4" />
 					</Button>
@@ -88,7 +97,7 @@ const PdfRenderer: FC<PdfRendererProps> = ({ url }) => {
 								}
 							}}
 						/>
-						<p className="text-zinc-700 text-small space-x-1">
+						<p className="text-zinc-700 text-sm space-x-1">
 							<span>/</span>
 							<span>{numPages ?? 'x'}</span>
 						</p>
@@ -96,12 +105,12 @@ const PdfRenderer: FC<PdfRendererProps> = ({ url }) => {
 
 					<Button
 						disabled={numPages === undefined || currPage === numPages}
-						variant="ghost"
-						aria-label="next page"
 						onClick={() => {
 							setCurrPage((prev) => (prev + 1 > numPages! ? numPages! : prev + 1));
 							setValue('page', String(currPage + 1));
 						}}
+						variant="ghost"
+						aria-label="next page"
 					>
 						<ChevronUp className="h-4 w-4" />
 					</Button>
@@ -129,33 +138,60 @@ const PdfRenderer: FC<PdfRendererProps> = ({ url }) => {
 						variant="ghost"
 						aria-label="rotate 90 degrees"
 					>
-						<RotateCcw className="h-4 w-4" />
+						<RotateCw className="h-4 w-4" />
 					</Button>
+
+					<PdfFullscreen fileUrl={url} />
 				</div>
 			</div>
 
 			<div className="flex-1 w-full max-h-screen">
-				<div ref={ref}>
-					<Document
-						loading={
-							<div className="flex justify-center">
-								<Loader2 className="my-24 h-6 w-6 animate-spin" />
-							</div>
-						}
-						onLoadError={() => {
-							toast({
-								title: 'Error',
-								description: 'Please try again later',
-								variant: 'destructive',
-							});
-						}}
-						onLoadSuccess={({ numPages }) => setNumPages(numPages)}
-						file={url}
-						className="max-h-full"
-					>
-						<Page width={width ? width : 1} pageNumber={currPage} />
-					</Document>
-				</div>
+				<SimpleBar autoHide={false} className="max-h-[calc(100vh-10rem)]">
+					<div ref={ref}>
+						<Document
+							loading={
+								<div className="flex justify-center">
+									<Loader2 className="my-24 h-6 w-6 animate-spin" />
+								</div>
+							}
+							onLoadError={() => {
+								toast({
+									title: 'Error loading PDF',
+									description: 'Please try again later',
+									variant: 'destructive',
+								});
+							}}
+							onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+							file={url}
+							className="max-h-full"
+						>
+							{isLoading && renderedScale ? (
+								<Page
+									width={width ? width : 1}
+									pageNumber={currPage}
+									scale={scale}
+									rotate={rotation}
+									key={'@' + renderedScale}
+								/>
+							) : null}
+
+							<Page
+								className={cn(isLoading ? 'hidden' : '')}
+								width={width ? width : 1}
+								pageNumber={currPage}
+								scale={scale}
+								rotate={rotation}
+								key={'@' + scale}
+								loading={
+									<div className="flex justify-center">
+										<Loader2 className="my-24 h-6 w-6 animate-spin" />
+									</div>
+								}
+								onRenderSuccess={() => setRenderedScale(scale)}
+							/>
+						</Document>
+					</div>
+				</SimpleBar>
 			</div>
 		</div>
 	);
