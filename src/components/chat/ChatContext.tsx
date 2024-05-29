e1d8a1e214a6716aed73a6642b698314d4c365d7
Ashot Israelyan <ashot.israelyan@tumo.org>
2024-05-29 15:36:54 +0400

    feat: message sending, saving and displaying implemented

--- src/components/chat/ChatContext.tsx
+++ src/components/chat/ChatContext.tsx
@@ -1,6 +1,8 @@
-import { ChangeEvent, createContext, FC, PropsWithChildren, useState } from 'react';
+import { ChangeEvent, createContext, FC, PropsWithChildren, useRef, useState } from 'react';
 import { useToast } from '@/components/ui/use-toast';
 import { useMutation } from '@tanstack/react-query';
+import { trpc } from '@/app/_trpc/client';
+import { INFINITE_QUERY_LIMIT } from '@/config/infinite-query';
 
 type StreamResponse = {
 	addMessage: () => void;
@@ -27,7 +29,11 @@ export const ChatContextProvider: FC<PropsWithChildren<ChatInputProps>> = ({
 	const [message, setMessage] = useState<string>('');
 	const [isLoading, setIsLoading] = useState<boolean>(false);
 
+	const utils = trpc.useContext();
+
 	const { toast } = useToast();
+
+	const backupMessage = useRef('');
 
 	const { mutate: sendMessage } = useMutation({
 		mutationFn: async ({ message }: { message: string }) => {
@@ -44,6 +50,129 @@ export const ChatContextProvider: FC<PropsWithChildren<ChatInputProps>> = ({
 			}
 
 			return response.body;
+		},
+		onMutate: async ({ message }) => {
+			backupMessage.current = message;
+			setMessage('');
+
+			await utils.getFileMessages.cancel();
+
+			const previousMessages = utils.getFileMessages.getInfiniteData();
+
+			utils.getFileMessages.setInfiniteData({ fileId, limit: INFINITE_QUERY_LIMIT }, (old) => {
+				if (!old) {
+					return {
+						pages: [],
+						pageParams: [],
+					};
+				}
+
+				let newPages = [...old.pages];
+
+				let latestPage = newPages[0]!;
+
+				latestPage.messages = [
+					{
+						createdAt: new Date().toISOString(),
+						id: crypto.randomUUID(),
+						text: message,
+						isUserMessage: true,
+					},
+					...latestPage.messages,
+				];
+
+				newPages[0] = latestPage;
+
+				return {
+					...old,
+					pages: newPages,
+				};
+			});
+
+			setIsLoading(true);
+
+			return {
+				previousMessages: previousMessages?.pages.flatMap((page) => page.messages) ?? [],
+			};
+		},
+		onSuccess: async (stream) => {
+			setIsLoading(false);
+
+			if (!stream) {
+				return toast({
+					title: 'There was a problem sending this message',
+					description: 'Please refresh this page and try again',
+					variant: 'destructive',
+				});
+			}
+
+			const reader = stream.getReader();
+			const decoder = new TextDecoder();
+			let done = false;
+
+			let accResponse = '';
+
+			while (!done) {
+				const { value, done: doneReading } = await reader.read();
+				done = doneReading;
+				const chunkValue = decoder.decode(value);
+
+				accResponse += chunkValue;
+
+				utils.getFileMessages.setInfiniteData({ fileId, limit: INFINITE_QUERY_LIMIT }, (old) => {
+					if (!old) return { pages: [], pageParams: [] };
+
+					let isAiResponseCreated = old.pages.some((page) =>
+						page.messages.some((message) => message.id === 'ai-response'),
+					);
+
+					let updatedPages = old.pages.map((page) => {
+						if (page === old.pages[0]) {
+							let updatedMessages;
+
+							if (!isAiResponseCreated) {
+								updatedMessages = [
+									{
+										createdAt: new Date().toISOString(),
+										id: 'ai-response',
+										text: accResponse,
+										isUserMessage: false,
+									},
+									...page.messages,
+								];
+							} else {
+								updatedMessages = page.messages.map((message) => {
+									if (message.id === 'ai-response') {
+										return {
+											...message,
+											text: accResponse,
+										};
+									}
+									return message;
+								});
+							}
+
+							return {
+								...page,
+								messages: updatedMessages,
+							};
+						}
+
+						return page;
+					});
+
+					return { ...old, pages: updatedPages };
+				});
+			}
+		},
+		onError: (_, __, context) => {
+			setMessage(backupMessage.current);
+			utils.getFileMessages.setData({ fileId }, { messages: context?.previousMessages ?? [] });
+		},
+		onSettled: async () => {
+			setIsLoading(false);
+
+			await utils.getFileMessages.invalidate({ fileId });
 		},
 	});
 
--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -1,15 +1,19 @@
 import { trpc } from '@/app/_trpc/client';
-import { FC, useRef } from 'react';
 import { INFINITE_QUERY_LIMIT } from '@/config/infinite-query';
-import { Loader2 } from 'lucide-react';
-import Message from '@/components/chat/Message';
+import { Loader2, MessageSquare } from 'lucide-react';
+import Skeleton from 'react-loading-skeleton';
+import Message from './Message';
+import { useContext, useEffect, useRef } from 'react';
+import { ChatContext } from './ChatContext';
 import { useIntersection } from '@mantine/hooks';
 
 interface MessagesProps {
 	fileId: string;
 }
 
-const Messages: FC<MessagesProps> = ({ fileId }) => {
+const Messages = ({ fileId }: MessagesProps) => {
+	const { isLoading: isAiThinking } = useContext(ChatContext);
+
 	const { data, isLoading, fetchNextPage } = trpc.getFileMessages.useInfiniteQuery(
 		{
 			fileId,
@@ -20,13 +24,6 @@ const Messages: FC<MessagesProps> = ({ fileId }) => {
 			keepPreviousData: true,
 		},
 	);
-
-	const lastMessageRef = useRef<HTMLDivElement>(null);
-
-	const { ref, entry } = useIntersection({
-		root: lastMessageRef.current,
-		threshold: 1,
-	});
 
 	const messages = data?.pages.flatMap((page) => page.messages);
 
@@ -41,11 +38,24 @@ const Messages: FC<MessagesProps> = ({ fileId }) => {
 		),
 	};
 
-	const combinedMessages = [...(true ? [loadingMessage] : []), ...(messages ?? [])];
+	const combinedMessages = [...(isAiThinking ? [loadingMessage] : []), ...(messages ?? [])];
+
+	const lastMessageRef = useRef<HTMLDivElement>(null);
+
+	const { ref, entry } = useIntersection({
+		root: lastMessageRef.current,
+		threshold: 1,
+	});
+
+	useEffect(() => {
+		if (entry?.isIntersecting) {
+			fetchNextPage();
+		}
+	}, [entry, fetchNextPage]);
 
 	return (
 		<div className="flex max-h-[calc(100vh-3.5rem-7rem)] border-zinc-200 flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
-			{combinedMessages?.length > 0 ? (
+			{combinedMessages && combinedMessages.length > 0 ? (
 				combinedMessages.map((message, i) => {
 					const isNextMessageSamePerson =
 						combinedMessages[i - 1]?.isUserMessage === combinedMessages[i]?.isUserMessage;
@@ -59,7 +69,7 @@ const Messages: FC<MessagesProps> = ({ fileId }) => {
 								key={message.id}
 							/>
 						);
-					} else {
+					} else
 						return (
 							<Message
 								message={message}
@@ -67,12 +77,20 @@ const Messages: FC<MessagesProps> = ({ fileId }) => {
 								key={message.id}
 							/>
 						);
-					}
 				})
 			) : isLoading ? (
-				<div></div>
+				<div className="w-full flex flex-col gap-2">
+					<Skeleton className="h-16" />
+					<Skeleton className="h-16" />
+					<Skeleton className="h-16" />
+					<Skeleton className="h-16" />
+				</div>
 			) : (
-				<div></div>
+				<div className="flex-1 flex flex-col items-center justify-center gap-2">
+					<MessageSquare className="h-8 w-8 text-blue-500" />
+					<h3 className="font-semibold text-xl">You&apos;re all set!</h3>
+					<p className="text-zinc-500 text-sm">Ask your first question to get started.</p>
+				</div>
 			)}
 		</div>
 	);
