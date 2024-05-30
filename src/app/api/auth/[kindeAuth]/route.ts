4a641c8ef2bf2ae4781eb3abe789b622d708534b
Ashot Israelyan <ashot.israelyan@tumo.org>
2024-05-30 11:35:02 +0400

    fix: kinde typescript

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,11 +1,7 @@
 import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server';
 import { NextRequest } from 'next/server';
 
-type Params = {
-	kindeAuth: string;
-};
-
-export async function GET(request: NextRequest, { params }: { params: Params }) {
+export async function GET(request: NextRequest, { params }: any) {
 	const endpoint = params.kindeAuth;
 	return handleAuth(request, endpoint);
 }
