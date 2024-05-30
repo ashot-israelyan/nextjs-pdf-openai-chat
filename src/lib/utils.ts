b9bc8fdb171b4e44c29a1e8f46b551707c164f76
Ashot Israelyan <ashot.israelyan@tumo.org>
2024-05-30 16:23:24 +0400

    feat: improve links and metadata

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx"
+import { Metadata } from 'next';
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
@@ -10,4 +11,48 @@ export function absoluteUrl(path: string) {
 	if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${path}`;
 
 	return `http://localhost:${process.env.PORT ?? 3000}${path}`;
+}
+
+export function constructMetadata({
+	title = "Quill - the SaaS for students",
+	description = "Quill is an open-source software to make chatting to your PDF files easy.",
+	image = "/thumbnail.png",
+	icons = "/favicon.ico",
+	noIndex = false
+}: {
+	title?: string
+	description?: string
+	image?: string
+	icons?: string
+	noIndex?: boolean
+} = {}): Metadata {
+	return {
+		title,
+		description,
+		openGraph: {
+			title,
+			description,
+			images: [
+				{
+					url: image,
+				}
+			]
+		},
+		twitter: {
+			card: 'summary_large_image',
+			title,
+			description,
+			images: [image],
+			creator: '#ashotisraelyan'
+		},
+		icons,
+		metadataBase: new URL('https://nextjs-sass-project.vercel.app'),
+		themeColor: '#fff',
+		...(noIndex && {
+			robots: {
+				index: false,
+				follow: false,
+			}
+		})
+	}
 }
